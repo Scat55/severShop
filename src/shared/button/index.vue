f7628af83f9702dc8995376f0abf9e86f6bc36c4
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-12-04 22:31:57 +0300

    feat: added Button and Icon components

---
+++ src/shared/button/index.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import Typography from '@/shared/typography/index.vue';
+
+interface Props {
+  color?: 'primary' | 'secondary' | 'grayscale' | 'error';
+  decoration?: 'default' | 'outline' | 'none';
+  size?: 'L' | 'M' | 'S';
+  disabled?: boolean;
+}
+
+const props = defineProps<Props>();
+
+const { color = 'primary', decoration = 'default', size = 'M', disabled = false } = props;
+
+const classes = ['button', `size_${size}`, `decoration_${decoration}`, `color_${color}`];
+</script>
+
+<template>
+  <button :class="classes" :disabled="disabled">
+    <slot name="leftIcon"></slot>
+    <Typography tagName="p" size="s" class="button__text">
+      <slot></slot>
+    </Typography>
+  </button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  width: 100%;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+
+  &.size_M {
+    grid-gap: 0.5rem;
+    padding: 0.5rem;
+  }
+  &.color_secondary {
+    background-color: var(--main-secondary);
+    color: var(--main-on-secondary);
+  }
+
+  &__text {
+    width: 100%;
+    text-align: center;
+  }
+}
+</style>
--- src/widgets/header/index.vue
+++ src/widgets/header/index.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import Container from '@/shared/container/index.vue';
 import Logo from '@/shared/logo/index.vue';
+import Button from '@/shared/button/index.vue';
+import Icon from '@/shared/icon/index.vue';
 </script>
 
 <template>
   <header class="header">
     <Container class="header__container">
       <Logo orientation="horizontal" bgColor="white" colorFull withText />
-      <p>mssm</p>
+
+      <div class="header__catalog">
+        <Button color="secondary">
+          <template v-slot:leftIcon><Icon type="menu" /></template>
+          Каталог
+        </Button>
+      </div>
     </Container>
   </header>
 </template>
@@ -23,7 +31,13 @@ import Logo from '@/shared/logo/index.vue';
   &__container {
     display: flex;
     align-items: center;
+
     height: 4.5rem;
+  }
+
+  &__catalog {
+    width: 8.75rem;
+    margin-left: 2.5rem;
   }
 }
 </style>
