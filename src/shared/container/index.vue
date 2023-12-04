05c8a40605b2efcc8e5484ab835f8a61c31eeec9
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-12-04 20:56:16 +0300

    feat: added Logo and Container components

---
+++ src/shared/container/index.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="container"><slot></slot></div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 75.5rem;
+  margin: 0 auto;
+}
+</style>
---
+++ src/widgets/header/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Container from '@/shared/container/index.vue';
+import Logo from '@/shared/logo/index.vue';
+</script>
+
+<template>
+  <header class="header">
+    <Container class="header__container">
+      <Logo orientation="horizontal" bgColor="white" colorFull withText />
+      <p>mssm</p>
+    </Container>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  position: sticky;
+  top: 0;
+  left: 0;
+  background: var(--surface);
+  box-shadow: var(--shadow-default-s);
+
+  &__container {
+    display: flex;
+    align-items: center;
+    height: 4.5rem;
+  }
+}
+</style>
